161ca93f3193d4c3da77b6b3dd2b46ace6438b96
mosespace <kisakyemoses50@gmail.com>
2023-11-09 15:51:37 +0300

    feat: initial commit

--- app/page.js
+++ app/page.js
@@ -1,12 +1,17 @@
 import Image from "next/image";
+import { categories } from "@/data";
+import Shop from "./components/Shop";
 import Hero from "./components/hero/Hero";
+import Product from "./components/Product";
 import FeaturedCategories from "./components/categories/FeaturedCategories";
 
 export default function Home() {
   return (
-    <main className='text-black w-full px-[10rem] flex flex-col gap-8 h-full overflow-hidden'>
+    <main className='text-black w-full px-5 lg:px-20 flex flex-col gap-8 h-full overflow-hidden'>
       <Hero />
-      <FeaturedCategories />
+      <FeaturedCategories categories={categories} />
+      <Product />
+      {/* <Shop /> */}
     </main>
   );
 }
