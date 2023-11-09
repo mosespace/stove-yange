c85e96bfaeaa56cf6c49e20441eb47e6220c14a9
mosespace <kisakyemoses50@gmail.com>
2023-11-09 15:51:47 +0300

    feat: restyled the alert

--- app/components/Alert.jsx
+++ app/components/Alert.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Alert() {
   return (
-    <div className='flex items-center justify-center bg-black text-white py-[7px] w-full'>
+    <div className='flex items-center justify-center bg-[#0E7490] text-white py-[7px] w-full'>
       <span className='text-xs px-8 cursor-pointer'>
         Always Giving Out Offers:
         <span className='text-[#ffa500] font-semibold'>
