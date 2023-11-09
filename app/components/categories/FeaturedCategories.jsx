674ad4901e8bd8479a1693c2b855e333397bf8ec
mosespace <kisakyemoses50@gmail.com>
2023-11-09 15:50:00 +0300

    feat: desktop version clear

--- app/components/categories/FeaturedCategories.jsx
+++ app/components/categories/FeaturedCategories.jsx
@@ -1,117 +1,53 @@
 "use client";
 import React from "react";
-import Carousel from "nuka-carousel";
-import Logo from "../../../public/stove-2.jpg";
-import { ChevronLeft, ChevronRight } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
+import Image from "next/image";
+import Carousel from "nuka-carousel";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
-export default function FeaturedCategories() {
-  const categories = [
-    {
-      path: "/stove/category/11245buwofjms8wm247ns",
-      image:
-        "https://images.unsplash.com/photo-1554381401-dc595be1d842?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8d2luZSUyMGJvdHRsZXN8ZW58MHx8MHx8fDA%3D",
-      catTitle: "Stove 3",
-    },
-    {
-      path: "/stove/category/11245buwofjms8wm247ns",
-      image:
-        "https://images.unsplash.com/photo-1610631787813-9eeb1a2386cc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8d2luZSUyMGJvdHRsZXxlbnwwfHwwfHx8MA%3D%3D",
-      catTitle: "Stove 4",
-    },
-    {
-      path: "/stove/category/11245buwofjms8wm247ns",
-      image:
-        "https://images.unsplash.com/photo-1586370434639-0fe43b2d32e6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8d2luZSUyMGJvdHRsZXxlbnwwfHwwfHx8MA%3D%3D",
-      catTitle: "Stove 5",
-    },
-    {
-      path: "/stove/category/11245buwofjms8wm247ns",
-      image:
-        "https://images.unsplash.com/photo-1544804066-ff04a3f1ab8b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHdpbmV8ZW58MHx8MHx8fDA%3D",
-      catTitle: "Stove 6",
-    },
-    {
-      path: "/stove/category/11245buwofjms8wm247ns",
-      image:
-        "https://images.unsplash.com/photo-1556442281-77c90134c61f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d2luZSUyMGJvdHRsZXN8ZW58MHx8MHx8fDA%3D",
-      catTitle: "Stove 7",
-    },
-    {
-      path: "/stove/category/11245buwofjms8wm247ns",
-      image:
-        "https://images.unsplash.com/photo-1592845148519-b0d41df97ac2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHdpbmUlMjBib3R0bGV8ZW58MHx8MHx8fDA%3D",
-      catTitle: "Stove 8",
-    },
-    {
-      path: "/stove/category/11245buwofjms8wm247ns",
-      image:
-        "https://images.unsplash.com/photo-1628187832510-94b4d90445af?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHdpbmUlMjBib3R0bGV8ZW58MHx8MHx8fDA%3D",
-      catTitle: "Stove 9",
-    },
-    {
-      path: "/stove/category/11245buwofjms8wm247ns",
-      image:
-        "https://images.unsplash.com/photo-1557682204-e53b55fd750c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d2luZSUyMGJvdHRsZXxlbnwwfHwwfHx8MA%3D%3D",
-      catTitle: "Stove 10",
-    },
-    {
-      path: "/stove/category/11245buwofjms8wm247ns",
-      image:
-        "https://images.unsplash.com/photo-1557682204-e53b55fd750c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d2luZSUyMGJvdHRsZXxlbnwwfHwwfHx8MA%3D%3D",
-      catTitle: "Stove 11",
-    },
-    {
-      path: "/stove/category/11245buwofjms8wm247ns",
-      image:
-        "https://images.unsplash.com/photo-1557682204-e53b55fd750c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d2luZSUyMGJvdHRsZXxlbnwwfHwwfHx8MA%3D%3D",
-      catTitle: "Stove 12",
-    },
-    {
-      path: "/stove/category/11245buwofjms8wm247ns",
-      image:
-        "https://images.unsplash.com/photo-1606298393349-4b117939dea0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHdpbmUlMjBib3R0bGVzfGVufDB8fDB8fHww",
-      catTitle: "Stove 13",
-    },
-  ];
+export default function FeaturedCategories({ categories }) {
   return (
-    <section className='my-5'>
+    <section className='my-5 overflow-hidden'>
       <div className='flex flex-col items-center py-5'>
         <h3 className='text-2xl font-bold'>Featured Categories</h3>
-        <div className=''>
-          <Carousel
-            renderCenterLeftControls={({ previousSlide }) => (
-              <button onClick={previousSlide}>
-                <ChevronLeft className='bg-white rounded-full w-9 h-9 text-black' />
-              </button>
-            )}
-            renderCenterRightControls={({ nextSlide }) => (
-              <button onClick={nextSlide}>
-                <ChevronRight className='bg-white rounded-full w-9 h-9 text-black' />
-              </button>
-            )}
-            slidesToShow={6}
-            cellSpacing={10}
-            className='mt-5 overflow-hidden'
-          >
-            {categories.map((category, i) => (
-              <Link href={category.path} className='w-full text-center'>
-                <Image
-                  className='object-cover object-center w-full h-48  rounded-lg'
-                  src={category.image}
-                  alt={category.title}
-                  width={500}
-                  height={500}
-                />
 
-                <div className='mt-2'>
-                  <h3 className='text-lg font-bold'>{category.catTitle}</h3>
-                </div>
-              </Link>
-            ))}
-          </Carousel>
-        </div>
+        <Carousel
+          wrapAround={false}
+          renderBottomCenterControls={null}
+          renderCenterLeftControls={({ previousSlide }) => (
+            <button onClick={previousSlide}>
+              <ChevronLeft className='bg-amber-500 rounded-full w-9 h-9 text-black' />
+            </button>
+          )}
+          renderCenterRightControls={({ nextSlide }) => (
+            <button onClick={nextSlide}>
+              <ChevronRight className='bg-amber-500 rounded-full w-9 h-9 text-black' />
+            </button>
+          )}
+          slidesToShow={6.5}
+          cellSpacing={20}
+          className='mt-5 rounded-lg'
+        >
+          {categories.map((category, id) => (
+            <Link
+              href={`/category/${category.slug}`}
+              key={category.id}
+              className='lg:w-full text-center'
+            >
+              <Image
+                className='object-cover object-center w-full h-40 lg:h-48  rounded-lg'
+                src={category.image}
+                alt={category.title}
+                width={500}
+                height={500}
+              />
+
+              <div className='mt-2'>
+                <h3 className='text-sm font-bold'>{category.title}</h3>
+              </div>
+            </Link>
+          ))}
+        </Carousel>
       </div>
     </section>
   );
