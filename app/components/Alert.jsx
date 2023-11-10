a1056bbeb9d5457b7eaff68bec62f2f756307b2a
mosespace <kisakyemoses50@gmail.com>
2023-11-10 20:40:32 +0300

    feat: initial commit

--- app/components/Alert.jsx
+++ app/components/Alert.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function Alert() {
   return (
-    <div className='flex items-center justify-center bg-[#0E7490] text-white py-[7px] w-full'>
-      <span className='text-xs px-8 cursor-pointer'>
+    <div className='flex justify-center bg-[#0E7490] text-white py-[7px] w-full'>
+      <p className='text-xs px-8 cursor-pointer'>
         Always Giving Out Offers:
         <span className='text-[#ffa500] font-semibold'>
           Next Offer In 1:45:00
         </span>
         (hrs)
-      </span>
+      </p>
     </div>
   );
 }
