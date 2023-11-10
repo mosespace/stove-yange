6ee9e8ed9d8b77075cd65e4fb6e4bc3a95dcac9c
mosespace <kisakyemoses50@gmail.com>
2023-11-10 21:27:19 +0300

    feat: completed hero section responsive

--- app/components/hero/Hero.jsx
+++ app/components/hero/Hero.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Image from "next/image";
 import BurnerOne from "../../../public/burner-01.jpg";
+import Link from "next/link";
 
 export default function Hero() {
   return (
-    <div className='bg-white my-5'>
-      <div className='flex flex-col lg:grid lg:grid-flow-col gap-8 overflow-hidden'>
-        <div className='row-span-full bg-teal-400 rounded-md relative isolate flex flex-col overflow-hidden px-8 justify-center w-full cursor-pointer h-[120px] lg:h-auto'>
+    <div className='bg-white lg:my-5'>
+      <div className='flex px-0 flex-col lg:grid lg:grid-flow-col gap-8 overflow-hidden'>
+        <div className='row-span-full bg-teal-400 md:rounded-md relative isolate flex flex-col overflow-hidden px-8 justify-center w-full cursor-pointer h-[400px] lg:h-auto'>
           {/* <video
             autoPlay
             loop
@@ -15,20 +16,23 @@ export default function Hero() {
           >
             <source src='vid.mp4' type='video/mp4' />
           </video> */}
-          <div className='absolute inset-0 object-cover object-center w-full h-full'>
+          <Link
+            href='/demo-url'
+            className='absolute inset-0 object-cover object-center w-full h-full'
+          >
             <Image
               src={BurnerOne}
               width={1000}
               height={1333}
               className='w-full h-full object-cover'
             />
-          </div>
-          <div className='absolute inset-0 bg-gradient-to-t from-[#0E7490]'></div>
-          <h3 className='z-10 mt-3 text-2xl lg:text-3xl font-bold text-white leading-snug'>
+          </Link>
+          <div className='hidden lg:block absolute inset-0 bg-gradient-to-t from-[#0E7490]'></div>
+          <h3 className='hidden lg:block z-10 mt-3 text-2xl lg:text-3xl font-bold text-white leading-snug'>
             Enjoy Up To UGX2500 <br />
             Off Fire Pits
           </h3>
-          <div className='z-10 overflow-hidden text-sm leading-6 text-gray-300 flex flex-col gap-4'>
+          <div className='hidden lg:flex z-10 overflow-hidden text-sm leading-6 text-gray-300 flex-col gap-4'>
             <p>Lowest prices of the year are here</p>
             <div>
               <button className='bg-white hover:bg-[#ffa500] hover:text-white text-black px-5 py-2 rounded-md'>
@@ -38,11 +42,11 @@ export default function Hero() {
           </div>
         </div>
 
-        <div className='row-span-auto flex gap-4 flex-col'>
-          <div className='relative isolate flex-grow rounded-md px-8 py-4 gap-5 flex flex-col bg-[#E7E7E7] justify-center bg-stove bg-cover bg-no-repeat '>
-            <div className='absolute inset-0 -z-10 bg-gradient-to-t from-black rounded-md'></div>
-            <h2 className='font-bold text-lg text-white'>
-              Win The Weekend With Pi Prime
+        <div className='px-5 row-span-auto flex gap-4 flex-col'>
+          <div className='relative isolate flex-grow rounded-md px-8 py-4 gap-5 flex flex-col bg-[#E7E7E7] justify-center bg-[url(/burner-02.jpg)] bg-cover bg-no-repeat '>
+            <div className='absolute inset-0 -z-10 bg-gradient-to-b from-[#0E7490] rounded-md'></div>
+            <h2 className='font-bold lg:text-lg text-2xl text-white'>
+              Win The Weekend With Premium pants
             </h2>
             <p className='text-sm text-white'>
               Enjoy Up To UGX25000 off Pi Prime
@@ -54,9 +58,11 @@ export default function Hero() {
             </div>
           </div>
 
-          <div className='relative isolate flex-grow rounded-md px-8 py-4 gap-5 flex flex-col bg-[#E7E7E7] justify-center bg-[url(/stove-2.jpg)]  bg-cover bg-no-repeat'>
-            <div className='absolute inset-0 -z-10 bg-gradient-to-t from-black rounded-md'></div>
-            <h2 className='font-bold text-lg text-white'>Bundle & Save</h2>
+          <div className='relative isolate flex-grow rounded-md px-8 py-4 gap-5 flex flex-col bg-[#E7E7E7] justify-center bg-[url(/burner-03.jpg)]  bg-cover bg-no-repeat'>
+            <div className='absolute inset-0 -z-10 bg-gradient-to-b from-[#ffa500] rounded-md'></div>
+            <h2 className='font-bold lg:text-lg text-2xl text-white'>
+              Never Get Cold, Get a Premium Hoods
+            </h2>
             <p className='text-sm text-white'>
               All the essentials to get the most out of your Solo Stove
             </p>
