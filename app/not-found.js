923eef7b431b4935f825e3b9b6beec44d358a469
mosespace <kisakyemoses50@gmail.com>
2023-11-10 21:09:22 +0300

    feat: changed visibility

--- app/not-found.js
+++ app/not-found.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <>
-      <div class='absolute left-0 right-0 top-0 bottom-0 z-50 overflow-hidden min-h-screen flex flex-grow items-center justify-center bg-gray-50'>
+      <div class='absolute left-0 right-0 top-0 bottom-0 z-[230] overflow-hidden min-h-screen flex flex-grow items-center justify-center bg-[#0E7490] px-8'>
         <div class='rounded-lg bg-white p-8 text-center shadow-xl'>
           <h1 class='mb-4 text-4xl font-bold'>404</h1>
           <p class='text-gray-600'>
