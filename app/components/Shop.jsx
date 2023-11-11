f45c67ec48c9b723404b417c24e915f9e9d224ac
mosespace <kisakyemoses50@gmail.com>
2023-11-11 03:27:49 +0300

    feat: added links to detailed page

--- app/components/Shop.jsx
+++ app/components/Shop.jsx
@@ -15,7 +15,7 @@
 //               </div>
 //               <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-4 md:gap-x-8 w-full'>
 //                 <div className='relative group flex justify-center items-center h-full w-full'>
-//                   <img
+//                   <Image
 //                     className='object-center object-cover h-full w-full'
 //                     src='https://i.ibb.co/ThPFmzv/omid-armin-m-VSb6-PFk-VXw-unsplash-1-1.png'
 //                     alt='girl-image'
@@ -27,7 +27,7 @@
 //                 </div>
 //                 <div className='flex flex-col space-y-4 md:space-y-8 mt-4 md:mt-0'>
 //                   <div className='relative group flex justify-center items-center h-full w-full'>
-//                     <img
+//                     <Image
 //                       className='object-center object-cover h-full w-full'
 //                       src='https://i.ibb.co/SXZvYHs/irene-kredenets-DDqx-X0-7v-KE-unsplash-1.png'
 //                       alt='shoe-image'
@@ -38,7 +38,7 @@
 //                     <div className='absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50' />
 //                   </div>
 //                   <div className='relative group flex justify-center items-center h-full w-full'>
-//                     <img
+//                     <Image
 //                       className='object-center object-cover h-full w-full'
 //                       src='https://i.ibb.co/Hd1pVxW/louis-mornaud-Ju-6-TPKXd-Bs-unsplash-1-2.png'
 //                       alt='watch-image'
@@ -50,7 +50,7 @@
 //                   </div>
 //                 </div>
 //                 <div className='relative group justify-center items-center h-full w-full hidden lg:flex'>
-//                   <img
+//                   <Image
 //                     className='object-center object-cover h-full w-full'
 //                     src='https://i.ibb.co/PTtRBLL/olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-1.png'
 //                     alt='girl-image'
@@ -61,12 +61,12 @@
 //                   <div className='absolute opacity-0 group-hover:opacity-100 transition duration-500 bottom-3 py-6 z-0 px-20 w-36 bg-white bg-opacity-50' />
 //                 </div>
 //                 <div className='relative group flex justify-center items-center h-full w-full mt-4 md:hidden md:mt-8 lg:hidden'>
-//                   <img
+//                   <Image
 //                     className='object-center object-cover h-full w-full hidden md:block'
 //                     src='https://i.ibb.co/6FjW19n/olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-2.png'
 //                     alt='girl-image'
 //                   />
-//                   <img
+//                   <Image
 //                     className='object-center object-cover h-full w-full md:hidden'
 //                     src='https://i.ibb.co/sQgHwHn/olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-1.png'
 //                     alt='olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-2'
@@ -78,12 +78,12 @@
 //                 </div>
 //               </div>
 //               <div className='relative group hidden md:flex justify-center items-center h-full w-full mt-4 md:mt-8 lg:hidden'>
-//                 <img
+//                 <Image
 //                   className='object-center object-cover h-full w-full hidden md:block'
 //                   src='https://i.ibb.co/6FjW19n/olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-2.png'
 //                   alt='girl-image'
 //                 />
-//                 <img
+//                 <Image
 //                   className='object-center object-cover h-full w-full sm:hidden'
 //                   src='https://i.ibb.co/sQgHwHn/olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-1.png'
 //                   alt='olive-tatiane-Im-Ez-F9-B91-Mk-unsplash-2'
@@ -101,25 +101,28 @@
 //   );
 // }
 import React from "react";
+import Image from "next/image";
+import Burner from "../../public/burner-01.jpg";
+import BurnerTwo from "../../public/burner-02.jpg";
 
 export default function Shop() {
   return (
-    <div className='flex justify-center items-center mb-5'>
+    <div className='flex justify-center items-center my-5'>
       <div className='md:w-auto grid lg:grid-cols-3 grid-cols-1 lg:gap-8 gap-6'>
         <div className='relative group'>
-          <img
-            className='lg:block hidden w-full'
-            src='https://i.ibb.co/SnL9NZV/pexels-two-dreamers-2345236-1.png'
+          <Image
+            className='lg:block hidden w-full object-cover object-center h-full rounded-l-lg '
+            src={Burner}
             alt='Women'
           />
-          <img
-            className='lg:hidden md:block hidden w-full'
-            src='https://i.ibb.co/PwMpH9g/pexels-two-dreamers-2345236-1-1.png'
+          <Image
+            className='lg:hidden md:block hidden w-full object-cover object-center h-full rounded-t-lg'
+            src={Burner}
             alt='Women'
           />
-          <img
-            className='w-full md:hidden'
-            src='https://i.ibb.co/Ks91wpt/pexels-two-dreamers-2345236-1.png'
+          <Image
+            className='w-full md:hidden object-cover object-center h-full rounded-t-lg'
+            src={Burner}
             alt='Women'
           />
           <div className='opacity-0 bg-gradient-to-t from-gray-800 via-gray-800 to-opacity-30 group-hover:opacity-50 absolute top-0 left-0 h-full w-full' />
@@ -131,30 +134,30 @@ export default function Shop() {
         </div>
         <div className=' lg:px-6 lg:py-0 md:py-16 md:px-24 py-16 px-6 flex flex-col justify-center items-center text-center bg-gray-100'>
           <h2 className=' font-semibold lg:text-4xl text-3xl lg:leading-normal leading-9 text-gray-800 lg:w-full md:w-7/12 w-full'>
-            Shop your Favourite Designers
+            Shop your Favorite Brands
           </h2>
           <p className=' font-medium text-base leading-6 text-gray-600 mt-4 lg:w-full md:w-7/12 w-full'>
-            We offer a huge colletion of premium clothing that are crafted with
+            We offer a huge collection of premium clothing that are crafted with
             excellence for our adored customers
           </p>
-          <button className='focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 hover:bg-gray-700 text-white text-base leading-4 bg-gray-800 lg:px-10 py-4 lg:w-auto w-72 mt-16'>
+          <button className='focus:outline-none focus:ring-2 focus:ring-offset-2 focus:bg-[#0E7490] hover:bg-[#0E7490]/90 text-white text-base leading-4 bg-[#0E7490] lg:px-10 py-4 lg:w-auto w-72 mt-16'>
             Discover More
           </button>
         </div>
         <div className='relative group'>
-          <img
-            className='lg:block hidden w-full'
-            src='https://i.ibb.co/gVMrdyp/pexels-mpumelelo-buthelezi-4503712-1-1.png'
+          <Image
+            className='lg:block hidden w-full object-cover object-center h-full rounded-r-lg'
+            src={BurnerTwo}
             alt='Man'
           />
-          <img
-            className='lg:hidden md:block hidden w-full'
-            src='https://i.ibb.co/hYmYWgT/pexels-two-dreamers-2345236-2.png'
+          <Image
+            className='lg:hidden md:block hidden w-full object-cover object-center  h-full rounded-r-lg'
+            src={BurnerTwo}
             alt='Man'
           />
-          <img
-            className='w-full md:hidden'
-            src='https://i.ibb.co/99kYP9T/pexels-two-dreamers-2345236-2.png'
+          <Image
+            className='w-full md:hidden object-cover object-center h-full rounded-b-lg'
+            src={BurnerTwo}
             alt='Man'
           />
           <div className='opacity-0 bg-gradient-to-t from-gray-800 via-gray-800 to-opacity-30 group-hover:opacity-50 absolute top-0 left-0 h-full w-full' />
@@ -172,7 +175,7 @@ export default function Shop() {
 //     <section
 //         className="p-5 md:p-0 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-10 items-start ">
 //         <section className="p-5 py-10 bg-purple-50 text-center transform duration-500 hover:-translate-y-2 cursor-pointer">
-//             <img src="https://www.dropbox.com/s/mlor33hzk73rh0c/x14423.png?dl=1" alt=""/>
+//             <Image src="https://www.dropbox.com/s/mlor33hzk73rh0c/x14423.png?dl=1" alt=""/>
 //             <div className="space-x-1 flex justify-center mt-10">
 //                 <svg className="w-4 h-4 mx-px fill-current text-orange-600" xmlns="http://www.w3.org/2000/svg"
 //                     viewBox="0 0 14 14">
@@ -212,7 +215,7 @@ export default function Shop() {
 //         </section>
 
 //         <section className="p-5 py-10 bg-green-50 text-center transform duration-500 hover:-translate-y-2 cursor-pointer">
-//             <img src="https://www.dropbox.com/s/8ymeus1n9k9bhpd/y16625.png?dl=1" alt=""/>
+//             <Image src="https://www.dropbox.com/s/8ymeus1n9k9bhpd/y16625.png?dl=1" alt=""/>
 //             <div className="space-x-1 flex justify-center mt-10">
 //                 <svg className="w-4 h-4 mx-px fill-current text-orange-600" xmlns="http://www.w3.org/2000/svg"
 //                     viewBox="0 0 14 14">
@@ -252,7 +255,7 @@ export default function Shop() {
 //         </section>
 
 //         <section className="p-5 py-10 bg-red-50 text-center transform duration-500 hover:-translate-y-2 cursor-pointer">
-//             <img src="https://www.dropbox.com/s/ykdro56f2qltxys/hh2774663-87776.png?dl=1" alt=""/>
+//             <Image src="https://www.dropbox.com/s/ykdro56f2qltxys/hh2774663-87776.png?dl=1" alt=""/>
 //             <div className="space-x-1 flex justify-center mt-10">
 //                 <svg className="w-4 h-4 mx-px fill-current text-orange-600" xmlns="http://www.w3.org/2000/svg"
 //                     viewBox="0 0 14 14">
@@ -292,7 +295,7 @@ export default function Shop() {
 //         </section>
 
 //         <section className="p-5 py-10 bg-blue-50 text-center transform duration-500 hover:-translate-y-2 cursor-pointer">
-//             <img src="https://www.dropbox.com/s/1fav310i2eqkdz8/tool2.png?dl=1" alt=""/>
+//             <Image src="https://www.dropbox.com/s/1fav310i2eqkdz8/tool2.png?dl=1" alt=""/>
 //             <div className="space-x-1 flex justify-center mt-10">
 //                 <svg className="w-4 h-4 mx-px fill-current text-orange-600" xmlns="http://www.w3.org/2000/svg"
 //                     viewBox="0 0 14 14">
