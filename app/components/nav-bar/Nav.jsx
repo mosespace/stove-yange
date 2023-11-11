323a033a36578e11bbbd169970b3f637594b2daf
mosespace <kisakyemoses50@gmail.com>
2023-11-11 14:47:27 +0300

    fixed: svg properties

--- app/components/nav-bar/Nav.jsx
+++ app/components/nav-bar/Nav.jsx
@@ -109,9 +109,9 @@ export default function Nav() {
                 >
                   <path
                     stroke='currentColor'
-                    stroke-linecap='round'
+                    strokeLinecap='round'
                     stroke-linejoin='round'
-                    stroke-width='2'
+                    strokeWidth='2'
                     d='m1 1 4 4 4-4'
                   />
                 </svg>
@@ -249,17 +249,17 @@ export default function Nav() {
             </div>
 
             <div className='flex flex-col w-full gap-4'>
-              <div class=' mb-4 flex w-full flex-wrap items-stretch'>
+              <div className=' mb-4 flex w-full flex-wrap items-stretch'>
                 <input
                   type='search'
-                  class='relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary'
+                  className='relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary'
                   placeholder='Search'
                   aria-label='Search'
                   aria-describedby='button-addon1'
                 />
 
                 <button
-                  class='relative z-[2] flex items-center rounded-r bg-primary px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg'
+                  className='relative z-[2] flex items-center rounded-r bg-primary px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-700 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg'
                   type='button'
                   id='button-addon1'
                   data-te-ripple-init
