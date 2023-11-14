19e2cf09d5c4494654d4df0d12c5ee69bf3dc611
mosespace <kisakyemoses50@gmail.com>
2023-11-14 08:34:18 +0300

    feat: added cart component

--- app/owinoug-cart/page.js
+++ app/owinoug-cart/page.js
@@ -1,1 +1,8 @@
-export default function page() {}
+import { divider } from "@nextui-org/react";
+import Cart from "../components/owinoug-cart/Cart";
+
+export default function page() {
+  <div>
+    <Cart />
+  </div>;
+}
