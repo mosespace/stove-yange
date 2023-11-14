78164411880bfce6629e0a5643d089ae39b06b52
mosespace <kisakyemoses50@gmail.com>
2023-11-14 08:34:38 +0300

    feat: intial commit to the cart

--- app/components/owinoug-cart/Cart.jsx
+++ app/components/owinoug-cart/Cart.jsx
@@ -1,5 +1,9 @@
 function Cart() {
-  return <div>Cart</div>;
+  return (
+    <div className='text-black px-5 w-full lg:px-20 flex flex-col'>
+      Welcome to the cart Page
+    </div>
+  );
 }
 
 export default Cart;
